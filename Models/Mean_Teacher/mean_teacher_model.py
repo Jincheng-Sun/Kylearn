b1717ddf153cd431721e3371cd5bb5ec22339d71
OEM Configuration (temporary user) <sjc951213@gmail.com>
2019-07-28 22:53:04 -0400

    add plain model to compare

--- Models/Mean_Teacher/mean_teacher_model.py
+++ Models/Mean_Teacher/mean_teacher_model.py
@@ -12,7 +12,8 @@ class Mean_Teacher_model_1d(Model):
 
     def __init__(self, ckpt_path, tsboard_path, network, input_shape, num_classes, feature_num, dev_num,
                  batch_size, lr, max_step=100000,
-                 wd=0.02, ema_decay=0.999, warmup_pos=0.4, coefficient=50, threshold=0.99, patience=10):
+                 wd=0.02, ema_decay=0.999, warmup_pos=0.4, coefficient=50,
+                 regression = False, threshold=0.99, patience=10):
         super().__init__(ckpt_path, tsboard_path)
 
         self.logger = log_down('train_log')
@@ -61,7 +62,10 @@ class Mean_Teacher_model_1d(Model):
 
         logits = self.classifier(network, scaled_input_x, num_classes=num_classes,
                               is_training=self.is_training)
-        self.logits = logits + output_bias
+        if regression:
+            self.logits = logits
+        else:
+            self.logits = logits + output_bias
 
         # Update BN before here
         post_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
@@ -77,21 +81,35 @@ class Mean_Teacher_model_1d(Model):
         logits_teacher = tf.stop_gradient(logits_t)
         logits_student = self.classifier(network, scaled_unlabeled_x, num_classes=num_classes,
                               is_training=self.is_training) # This part better use augmented `unlabeled_x`
-        loss_consistency = tf.reduce_mean((tf.nn.softmax(logits_teacher) - tf.nn.softmax(logits_student)) ** 2, -1)
-        self.loss_c = tf.reduce_mean(loss_consistency)
 
-        onehot_y = tf.cast(self.input_y, tf.int32)
-        self.onehot_y = tf.one_hot(onehot_y, num_classes)
 
-        with tf.variable_scope('error'):
-            self.proba = tf.nn.sigmoid(self.logits)
-            self.loss = tf.reduce_mean(
-                # use `sparse`, no need to one-hot the `input_y`
-                tf.nn.softmax_cross_entropy_with_logits_v2(labels = self.onehot_y, logits = self.logits))
-            self.prediction = tf.nn.softmax(self.logits)
-            self.prediction = tf.argmax(self.prediction, 1)
-            self.real = tf.argmax(self.onehot_y, 1)
-            self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.prediction, self.real), tf.float32))
+        if regression:
+            input_y = tf.expand_dims(self.input_y, axis = -1)
+            self.loss_c = tf.reduce_mean(tf.square(tf.nn.sigmoid(logits_teacher) - tf.nn.sigmoid(logits_student)))
+            with tf.variable_scope('error'):
+                self.proba = tf.nn.sigmoid(self.logits)
+                self.error = self.proba - input_y
+                self.loss = tf.reduce_mean(tf.square(self.error))
+                threshold = tf.constant(threshold)
+                condition = tf.greater_equal(self.proba, threshold)
+                self.prediction = tf.where(condition, tf.ones_like(self.proba), tf.zeros_like(self.proba),
+                                           name='prediction')
+                self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.prediction, input_y), tf.float32))
+        else:
+            onehot_y = tf.cast(self.input_y, tf.int32)
+            self.onehot_y = tf.one_hot(onehot_y, num_classes)
+            with tf.variable_scope('error'):
+                loss_consistency = tf.reduce_mean(
+                    (tf.nn.softmax(logits_teacher) - tf.nn.softmax(logits_student)) ** 2, -1)
+                self.loss_c = tf.reduce_mean(loss_consistency)
+                self.proba = tf.nn.sigmoid(self.logits)
+                self.loss = tf.reduce_mean(
+                    # use `sparse`, no need to one-hot the `input_y`
+                    tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.onehot_y, logits=self.logits))
+                self.prediction = tf.nn.softmax(self.logits)
+                self.prediction = tf.argmax(self.prediction, 1)
+                self.real = tf.argmax(self.onehot_y, 1)
+                self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.prediction, self.real), tf.float32))
 
         tf.summary.scalar('losses/xe', self.loss)
         tf.summary.scalar('losses/mt', self.loss_c)
@@ -217,7 +235,7 @@ class Mean_Teacher_model_2d(Model):
 
     def __init__(self, ckpt_path, tsboard_path, network, input_shape, num_classes, feature_num, dev_num,
                  batch_size, lr, max_step=100000,
-                 wd=0.02, ema_decay=0.999, warmup_pos=0.4, coefficient=50, threshold=0.99, patience=10):
+                 wd=0.02, ema_decay=0.999, warmup_pos=0.4, coefficient=50, threshold=0.5, patience=10):
         super().__init__(ckpt_path, tsboard_path)
 
         self.logger = log_down('train_log')
@@ -232,7 +250,7 @@ class Mean_Teacher_model_2d(Model):
         with tf.variable_scope("input"):
             self.input_x = tf.placeholder(tf.float32, [None] + input_shape, name='features')
             self.input_dev = tf.placeholder(tf.float32, [None,], name='dev_type')
-            self.input_y = tf.placeholder(tf.float32, [None,], name='alarm')
+            self.input_y = tf.placeholder(tf.float32, [None, 1], name='alarm')
             self.unlabel_x = tf.placeholder(tf.float32, [None] + input_shape, name='unlabeled_features')
             self.unlabel_dev = tf.placeholder(tf.float32, [None,], name='unlabeled_dev_type')
             self.is_training = tf.placeholder(dtype=tf.bool, shape=(), name='is_training')
@@ -247,13 +265,8 @@ class Mean_Teacher_model_2d(Model):
             input_matrices_un = tf.gather(self.input_attention, tf.cast(self.unlabel_dev, tf.int32))
             input_matrices_un = tf.expand_dims(input_matrices_un, axis=-1)
 
-        with tf.variable_scope('bias_attention'):
-            output_attn = tf.get_variable('output_attention', [dev_num, num_classes], trainable=True, initializer=tf.initializers.zeros)
-            self.output_attention = tf.nn.leaky_relu(output_attn)
-            output_bias = tf.gather(self.output_attention, tf.cast(self.input_dev, tf.int32))
 
-
-        # apply attention matrix
+        # # apply attention matrix
         scaled_input_x = tf.multiply(self.input_x, input_matrices)
         scaled_unlabeled_x = tf.multiply(self.unlabel_x, input_matrices_un)
         # #not applying attention matrix
@@ -266,7 +279,7 @@ class Mean_Teacher_model_2d(Model):
 
         logits = self.classifier(network, scaled_input_x, num_classes=num_classes,
                               is_training=self.is_training)
-        self.logits = logits + output_bias
+        self.logits = logits
 
         # Update BN before here
         post_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
@@ -282,21 +295,18 @@ class Mean_Teacher_model_2d(Model):
         logits_teacher = tf.stop_gradient(logits_t)
         logits_student = self.classifier(network, scaled_unlabeled_x, num_classes=num_classes,
                               is_training=self.is_training) # This part better use augmented `unlabeled_x`
-        loss_consistency = tf.reduce_mean((tf.nn.softmax(logits_teacher) - tf.nn.softmax(logits_student)) ** 2, -1)
+        loss_consistency = tf.reduce_mean((tf.nn.sigmoid(logits_teacher) - tf.nn.sigmoid(logits_student)) ** 2, -1)
         self.loss_c = tf.reduce_mean(loss_consistency)
-
-        onehot_y = tf.cast(self.input_y, tf.int32)
-        self.onehot_y = tf.one_hot(onehot_y, num_classes)
 
         with tf.variable_scope('error'):
             self.proba = tf.nn.sigmoid(self.logits)
-            self.loss = tf.reduce_mean(
-                # use `sparse`, no need to one-hot the `input_y`
-                tf.nn.softmax_cross_entropy_with_logits_v2(labels = self.onehot_y, logits = self.logits))
-            self.prediction = tf.nn.softmax(self.logits)
-            self.prediction = tf.argmax(self.prediction, 1)
-            self.real = tf.argmax(self.onehot_y, 1)
-            self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.prediction, self.real), tf.float32))
+            self.error = self.proba - self.input_y
+            self.loss = tf.reduce_mean(tf.square(self.error))
+            threshold = tf.constant(threshold)
+            condition = tf.greater_equal(self.proba, threshold)
+            self.prediction = tf.where(condition, tf.ones_like(self.proba), tf.zeros_like(self.proba), name='prediction')
+            self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.prediction, self.input_y), tf.float32))
+
 
         tf.summary.scalar('losses/xe', self.loss)
         tf.summary.scalar('losses/mt', self.loss_c)
@@ -340,8 +350,10 @@ class Mean_Teacher_model_2d(Model):
                                                        evaluation_span=100,
                                                        max_step=self.max_step)  # batch*evaluation_span = dataset size = one epoch
 
-        for labeled, unlabeled in dataset.training_generator(batch_size=self.batch_size, portion= 0.5):
-
+        for labeled_pos, labeled_neg, unlabeled in dataset.training_generator(pos=round(self.batch_size*0.25),
+                                                             neg=round(self.batch_size*0.25),
+                                                             un=round(self.batch_size*0.5)):
+            labeled = np.concatenate([labeled_pos, labeled_neg], axis=0)
             accuracy, loss, lossc, wm, _ = self.run([self.accuracy, self.loss,
                                                      self.loss_c, self.warmup,
                                                  self.train_op],
@@ -415,5 +427,152 @@ class Mean_Teacher_model_2d(Model):
         return proba
 
     def get_attn_matrix(self):
-        scaling_attention, bias_attention = self.run([self.input_attention, self.output_attention])
-        return scaling_attention, bias_attention
+        scaling_attention = self.run([self.input_attention])
+        return scaling_attention
+
+class plain_model_2d(Model):
+
+    def __init__(self, ckpt_path, tsboard_path, network, input_shape, num_classes, feature_num, dev_num,
+                 batch_size, lr, max_step=100000,
+                 wd=0.02, ema_decay=0.999, warmup_pos=0.4, coefficient=50, threshold=0.5, patience=10):
+        super().__init__(ckpt_path, tsboard_path)
+
+        self.logger = log_down('train_log')
+        self.batch_size = batch_size
+        self.patience = 0
+        self.patience_max = patience
+        self.max_step = max_step
+
+        wd *= lr
+        self.warmup = tf.clip_by_value(tf.to_float(self.global_step * batch_size) / (warmup_pos * (2469001 * 1024)), 0, 1)
+
+        with tf.variable_scope("input"):
+            self.input_x = tf.placeholder(tf.float32, [None] + input_shape, name='features')
+            self.input_dev = tf.placeholder(tf.float32, [None,], name='dev_type')
+            self.input_y = tf.placeholder(tf.float32, [None, 1], name='alarm')
+            self.unlabel_x = tf.placeholder(tf.float32, [None] + input_shape, name='unlabeled_features')
+            self.unlabel_dev = tf.placeholder(tf.float32, [None,], name='unlabeled_dev_type')
+            self.is_training = tf.placeholder(dtype=tf.bool, shape=(), name='is_training')
+
+        m = input_shape[0]
+        with tf.variable_scope('scaling_attention'):
+            input_attn = tf.get_variable('input_attention', [dev_num, m, feature_num], trainable=True,
+                                         initializer=tf.initializers.ones)
+            self.input_attention = tf.nn.leaky_relu(input_attn)
+            input_matrices = tf.gather(self.input_attention, tf.cast(self.input_dev, tf.int32))
+            input_matrices = tf.expand_dims(input_matrices, axis=-1)
+
+        # Dot product to scale the input
+        scaled_input_x = tf.multiply(self.input_x, input_matrices)
+
+        self.logits = self.classifier(network, scaled_input_x, num_classes=num_classes,
+                              is_training=self.is_training)
+
+
+        with tf.variable_scope('error'):
+            self.proba = tf.nn.sigmoid(self.logits)
+            self.error = self.proba - self.input_y
+            self.loss = tf.reduce_mean(tf.square(self.error))
+            threshold = tf.constant(threshold)
+            condition = tf.greater_equal(self.proba, threshold)
+            self.prediction = tf.where(condition, tf.ones_like(self.proba), tf.zeros_like(self.proba), name='prediction')
+            self.accuracy = tf.reduce_mean(tf.cast(tf.equal(self.prediction, self.input_y), tf.float32))
+
+
+        tf.summary.scalar('losses/xe', self.loss)
+        self.best_loss = 100000
+
+        optimizer = tf.train.AdamOptimizer(learning_rate=lr)
+
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        train_op = optimizer.minimize(self.loss, global_step=self.global_step)
+        self.train_op = tf.group([train_op, update_ops])
+
+        self.saver = tf.train.Saver(max_to_keep=self.patience_max)
+        self.writer = tf.summary.FileWriter(self.tensorboard_path)
+
+    def classifier(self, network, input, num_classes, is_training, name='logits', getter=None):
+        with tf.variable_scope(name, reuse=tf.AUTO_REUSE, custom_getter=getter):
+            net = network.network(inputs=input, num_classes=num_classes, is_training=is_training)
+
+        return net
+
+    def initialize_variables(self, **kwargs):
+        init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+        self.run(init_op)
+
+    def run(self, *args, **kwargs):
+        return self.session.run(*args, **kwargs)
+
+    def train(self, dataset):
+        self.training_control = utils.training_control(self.global_step, print_span=10,
+                                                       evaluation_span=100,
+                                                       max_step=self.max_step)  # batch*evaluation_span = dataset size = one epoch
+
+        for labeled_pos, labeled_neg, unlabeled in dataset.training_generator(pos=round(self.batch_size*0.75),
+                                                             neg=round(self.batch_size*0.25),
+                                                             un=round(self.batch_size*0.5)):
+            labeled = np.concatenate([labeled_pos, labeled_neg], axis=0)
+            accuracy, loss, wm, _ = self.run([self.accuracy, self.loss,
+                                                      self.warmup,
+                                                 self.train_op],
+                                        feed_dict={self.input_x: labeled['x'],
+                                                   self.input_dev: labeled['dev'],
+                                                   self.input_y: labeled['y'],
+                                                   self.is_training: True})
+            step_control = self.run(self.training_control)
+            if step_control['time_to_print']:
+                print('train_loss = ' + str(loss) +
+                      '     train_acc= '+str(accuracy) +
+                      '     step ' + str(step_control['step']) +
+                      '     warmup' + str(wm))
+            if step_control['time_to_stop']:
+                self.save_checkpoint()
+                break
+            if step_control['time_to_evaluate']:
+                self.evaluate(dataset.val_set)
+                self.save_checkpoint()
+
+
+    def evaluate(self, val_data):
+        step, loss, accuracy = self.run([self.global_step, self.loss, self.accuracy],
+                                 feed_dict={self.input_x: val_data['x'],
+                                            self.input_dev: val_data['dev'],
+                                            self.input_y: val_data['y'],
+                                            self.is_training: False})
+        self.logger.info('val_loss= ' + str(loss) + '    val_acc= '+str(accuracy)+'          round: ' + str(step))
+
+
+    def get_prediction(self, data, is_training=False):
+        prediction = self.run(self.prediction, feed_dict={
+            self.input_x: data['x'],
+            self.input_dev: data['dev'],
+            self.is_training: is_training
+        })
+        return prediction
+
+    def get_accuracy(self, data, is_training=False):
+        accuracy = self.run(self.accuracy, feed_dict = {
+            self.input_x: data['x'],
+            self.input_dev: data['dev'],
+            self.input_y: data['y'],
+            self.is_training: is_training
+        })
+        return accuracy
+
+    def get_logits(self, data, is_training=False):
+        logits = self.run([self.logits], feed_dict={
+            self.input_x: data['x'],
+            self.input_dev: data['dev'],
+            self.is_training: is_training
+        })
+        return logits
+
+    def get_proba(self, data, is_training=False):
+        proba = self.run(self.proba, feed_dict={
+            self.input_x: data['x'],
+            self.input_dev: data['dev'],
+            self.is_training: is_training
+        })
+        return proba
+
